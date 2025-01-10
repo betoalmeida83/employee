c2bf86d9178784e687f522eeb641530da064779f
betoalmeida83 <roberto.almeida5@fatec.sp.gov.br>
2025-01-09 22:57:40 -0300

    Initial Commit

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -1,4 +1,41 @@
 package application;
 
+import entities.Employee;
+
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Program {
+
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner input = new Scanner(System.in);
+
+        Employee employee = new Employee();
+
+        System.out.print("Name: ");
+        employee.name = input.nextLine();
+        System.out.print("Gross salary: ");
+        employee.grossSalary = input.nextDouble();
+        System.out.print("Tax: ");
+        employee.tax = input.nextDouble();
+
+        System.out.println();
+        System.out.print("Employee: " + employee.name);
+        System.out.printf(", $ %.2f%n", employee.netSalary());
+
+        System.out.println();
+        System.out.print("Which percentage to increase salary? ");
+        double percentage = input.nextDouble();
+
+        employee.increaseSalary(percentage);
+
+        System.out.println();
+        System.out.print("Update data: " + employee.name);
+        System.out.printf(", $ %.2f", employee.netSalary());
+
+        input.close();
+
+    }
 }
